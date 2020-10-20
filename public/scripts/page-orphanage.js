edf5f0f6d6468a6d8c072a59ceedaec55cc70587
Renan Vieira Guedes <renanv.guedes7@gmail.com>
2020-10-20 16:04:40 -0300

    Init

---
+++ public/scripts/page-orphanage.js
@@ -0,0 +1,62 @@
+//tipos de dados
+//String ""
+//Number 01
+//Boolean true or false
+//Object {}
+//Array []
+
+const options = {
+    dragging: false,
+    touchZoom: false,
+    doubleClickZoom: false,
+    scrollWhelZoom: false,
+    zoomControl: false,
+}
+
+//get values from html
+const lat = document.querySelector('span[data-lat]').dataset.lat
+const lng = document.querySelector('span[data-lng]').dataset.lng
+
+
+//create map
+const map = L.map('mapid', options).setView([lat,lng], 15);
+
+//create and add a tileLayer
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+
+//create icon
+const icon = L.icon({
+    iconUrl: "/images/map-marker.svg",
+    iconSize: [58, 68],
+    iconAnchor: [29, 68],
+    popupAnchor: [170, 2]
+});
+
+//create and add marker
+L.marker([lat, lng], {icon})
+.addTo(map)
+
+//image gallery 
+
+function selectImage(event) {
+    const button = event.currentTarget
+
+    //remover todas as classes  .active
+    const buttons = document.querySelectorAll(".images button")
+    buttons.forEach(removeActiveClass)
+
+    function removeActiveClass(button) {
+        button.classList.remove("active")
+    }
+
+    //selecionar a imagem clicada
+    const image = button.children[0]
+    const imageContainer = document.querySelector(".orphanage-details > img")
+
+    //atualizar o conatiner de imagem
+    imageContainer.src = image.src
+
+    //adicionar a casse .active para esse botão
+    button.classList.add('active')
+
+}
